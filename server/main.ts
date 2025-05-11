c42f56311ca2e1230cfc59ef1187f86f81c0cfcd
haike0513 <haike0513@gmail.com>
2025-05-12 00:12:26 +0800

    feat: add logger

--- server/main.ts
+++ server/main.ts
@@ -2,9 +2,13 @@
 
 import {Hono} from "hono"
 import {serveStatic} from "hono/deno"
+import { logger } from 'hono/logger'
+
 import middlewareReactRouter from "./middleware/reactRouter.ts"
 
 const app = new Hono()
+
+app.use(logger())
 
 app.use("/assets/*", serveStatic({root: "./build/client"}))
 app.use("*", middlewareReactRouter)
