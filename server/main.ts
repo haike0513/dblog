ed9b2a7689ef4c0c4bceaf1e4294ebe6697fd250
haike0513 <haike0513@gmail.com>
2025-05-12 11:28:44 +0800

    add hono ssr

--- server/main.ts
+++ server/main.ts
@@ -1,16 +1,4 @@
-// server/main.js
-
-import {Hono} from "hono"
-import {serveStatic} from "hono/deno"
-import { logger } from 'hono/logger'
-
-import middlewareReactRouter from "./middleware/reactRouter.ts"
-
-const app = new Hono()
-
-app.use(logger())
-
-app.use("/assets/*", serveStatic({root: "./build/client"}))
-app.use("*", middlewareReactRouter)
+import app from "./app.tsx"
 
 Deno.serve(app.fetch)
+// export default app;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
-import deno from "@deno/vite-plugin"
+import deno from "@deno/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import devServer from "@hono/vite-dev-server";
 
 export default defineConfig(({ isSsrBuild }) => ({
+  build: {
+    target: "ESNEXT",
+  },
   // build: {
   //   rollupOptions: isSsrBuild
   //     ? {
@@ -12,11 +16,19 @@ export default defineConfig(({ isSsrBuild }) => ({
   //       }
   //     : undefined,
   // },
-  plugins: [deno(), tailwindcss(), reactRouter(), tsconfigPaths()],
-      ssr: {
-      resolve: {
-        conditions: ["module", "deno", "node", "development|production"],
-        externalConditions: ["deno", "node"],
-      },
+  plugins: [
+    deno(),
+    tailwindcss(),
+    reactRouter(),
+    // tsconfigPaths(),
+    devServer({
+      entry: "./server/app.tsx", // The file path of your application.
+    }),
+  ],
+  ssr: {
+    resolve: {
+      conditions: ["module", "deno", "node", "development|production"],
+      externalConditions: ["deno", "node"],
     },
+  },
 }));
